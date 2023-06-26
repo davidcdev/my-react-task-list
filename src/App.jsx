eb25e8efd3a7427183264cd3661baee94f2778a9
DavidCDev <davidcastellanos.dev@gmail.com>
2023-06-26 16:52:52 -0500

    lifting state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,85 @@
-import { useState } from 'react'
 import './App.css'
+import { useState, useEffect } from 'react'
 import { Header } from "./components/Header"
 import { TaskList } from './components/TaskList'
+import { notifyCompleted, notifyDeleted, notifySaved } from "./logic/notifications";
+import { useUpdateTasks } from './hooks/useUpdateTasks';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [ task, setTask ] = useState([]);
+  const [complete, setComplete] = useState(false);
+
+  const saveTask = (inputValue) => {
+       
+    inputValue = {
+        id: Date.now(),
+        description: `${inputValue}`,
+        isCompleted: false 
+    }
+   
+    let newTask = [...task];
+    newTask = [...newTask, inputValue];
+    setTask(newTask);
+    localStorage.setItem("task", JSON.stringify(newTask));
+    notifySaved();
+  }
+
+  useEffect(() => {
+      const localStorageData = localStorage.getItem("task");
+      const storedTask = JSON.parse(localStorageData);
+      console.log(storedTask)
+      if (storedTask) {
+          setTask(storedTask);
+      }
+  }, []);
+
+  const handleKeyDown = (e) => {
+      if (e.key === 'Enter' && inputValue !== '') {
+          saveTask(inputValue)
+          setInputValue('')
+      }
+  }
+
+  const handleAddClick = (e) => {
+      if (inputValue !== '') {
+          saveTask(inputValue)
+          setInputValue('')
+      }
+  }
+
+  const handleCompleteClick = (newCompleted, id) => {
+    if (newCompleted.isCompleted === false) {
+      setComplete(newCompleted.isCompleted = true)
+      let newTask = [...task];
+      newTask = [...newTask.filter(e => e.id != id), newCompleted];
+      setTask(newTask);
+      localStorage.setItem("task", JSON.stringify(newTask));   
+      notifyCompleted();
+    }
+  }
+
+  const handleDeleteClick = (id) => {
+    let newTask = [...task];
+    const list = newTask.filter(e => e.id != id)
+    setTask(list);
+    localStorage.setItem("task", JSON.stringify(list));
+    notifyDeleted();
+  }
+
   return (
     <>
       <Header />
-      <TaskList />
+      <TaskList 
+        inputValue={inputValue}
+        onInputValueChange={setInputValue}
+        handleKeyDown={handleKeyDown}
+        handleAddClick={handleAddClick}
+        task={task}
+        complete={complete}
+        handleCompleteClick={handleCompleteClick}
+        handleDeleteClick={handleDeleteClick}
+      />
     </>
   )
 }
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,79 +1,16 @@
-import { useState, useEffect } from "react";
 import { Task } from "./Task"
-import { notifyCompleted, notifyDeleted, notifySaved } from "../logic/notifications";
 import { Toaster } from "react-hot-toast";
 
 
-export function TaskList () {
-    const [ task, setTask ] = useState([]);
-    
-    const saveTask = (inputValue) => {
-       
-        inputValue = {
-            id: Date.now(),
-            description: `${inputValue}`,
-            isCompleted: false 
-        }
-       
-        let newTask = [...task];
-
-        newTask = [...newTask, inputValue];
-
-        setTask(newTask);
-
-        localStorage.setItem("task", JSON.stringify(newTask));
-
-        notifySaved();
-    }
-
-    useEffect(() => {
-        const localStorageData = localStorage.getItem("task");
-        const storedTask = JSON.parse(localStorageData);
-        console.log(storedTask)
-        if (storedTask) {
-            setTask(storedTask);
-        }
-    }, []);
-
-    /* console.log(task) */
-
-    const [inputValue, setInputValue] = useState('');
-
-    const handleKeyDown = (e) => {
-        if (e.key === 'Enter' && inputValue !== '') {
-            saveTask(inputValue)
-            setInputValue('')
-        }
-    }
-
-    const handleAddClick = (e) => {
-        if (inputValue !== '') {
-            saveTask(inputValue)
-            setInputValue('')
-        }
-    }
-
-    const [complete, setComplete] = useState(false)
-
-    const handleCompleteClick = (newCompleted, id) => {
-        if (newCompleted.isCompleted === false) {
-        setComplete(newCompleted.isCompleted = true)
-        let newTask = [...task];
-        newTask = [...newTask.filter(e => e.id != id), newCompleted];
-        setTask(newTask);
-        localStorage.setItem("task", JSON.stringify(newTask));   
-        notifyCompleted();
-        }
-    }
-    /* console.log(task) */
-    
-    const handleDeleteClick = (id) => {
-        let newTask = [...task];
-        const list = newTask.filter(e => e.id != id)
-        setTask(list);
-        localStorage.setItem("task", JSON.stringify(list));
-        notifyDeleted();
-    }
+export function TaskList ({
+    inputValue,
+    onInputValueChange, 
+    handleKeyDown,
+    handleAddClick, 
+    task, 
+    complete,
+    handleCompleteClick,
+    handleDeleteClick }) {
 
     return (
         <section>
@@ -83,14 +20,14 @@ export function TaskList () {
                     placeholder="Add A New Task" 
                     className="new-task" 
                     value={inputValue}
-                    onChange={(e) => { setInputValue(e.target.value) }}
+                    onChange={(e) => { onInputValueChange(e.target.value) }}
                     onKeyDown={ handleKeyDown }
                     autoFocus
                 />
                 <button 
                     type="submit" 
                     className="add-button" 
-                    onChange={(e) => { setInputValue(e.target.value) }}
+                    onChange={(e) => { onInputValueChange(e.target.value) }}
                     onClick={ handleAddClick }
                 >
                     +
