621e4b47b8a7f4da1bbdff186f76781b0f357bd5
DavidCDev <davidcastellanos.dev@gmail.com>
2023-07-11 11:55:18 -0500

    minor change for responsive view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Flex, Heading, Stack } from '@chakra-ui/react'
 export function Header () {
     return (
         <Flex flexDirection='column' justifyContent='center' alignItems='center'>
-        <Stack spacing={4} mt={{ base:'200px', md:'120px'}} >
+        <Stack spacing={4} mt={'120px'} >
             <Heading as='h2'size='xl' textAlign='center' fontSize={{ base: '32px', md: '40px' }}>
                 Hi! Welcome to
             </Heading>
--- src/components/TaskCounter.jsx
+++ src/components/TaskCounter.jsx
@@ -5,13 +5,13 @@ export function TaskCounter ({ taskList }) {
     const [parent] = useAutoAnimate();
 
     return (
-        <Flex flexDir={{base:'column', lg:'row'}} justify='space-evenly' align='center' mt='100px' w={{md:'50%'}} ref={parent}>
+        <Flex flexDir={{base:'column', lg:'row'}} justify='space-evenly' alignItems='center' my={{base: '50px', lg:'100px'}} w={{md:'50%'}} ref={parent}>
             <Card mt='40px' w={{base:'300px', md:'350px'}} boxShadow='md' _hover={{transform:'translateY(-1px)'}}>
                 <CardBody fontSize={{ base: '16px', md:'18px' }}>
                         You have {taskList.filter((task) => task.isCompleted === false).length} pending tasks for today.
                 </CardBody>
             </Card>
-            <Card mt='40px' w={{base:'300px', md:'350px'}} boxShadow='md' _hover={{transform:'translateY(-1px)'}}>
+            <Card mt={{base:'32px', lg:'40px'}} w={{base:'300px', md:'350px'}} boxShadow='md' _hover={{transform:'translateY(-1px)'}}>
                 <CardBody fontSize={{ base: '16px', md:'18px' }}>
                         You have {taskList.filter((task) => task.isCompleted === true).length} completed tasks today.
                 </CardBody>
